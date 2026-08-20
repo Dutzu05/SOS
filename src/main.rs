40a64585cbd41f6c71f0964238df80629eae7919
Dutzu05 <vladlusca05@gmail.com>
2026-08-20 12:04:29 +0300

    Lesson 3 - multiple apps

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,34 @@ enum BusMessage {
 
 fn main() {
     let (tx, rx) = mpsc::channel::<BusMessage>();
-
-    let sensor_app = thread::spawn(move || {
-        let msg = BusMessage::Telemetry { sensor_id: 1, value: 23.7 };
-        println!("[sensor_app] sending: {:?}", msg);
+    let tx2 = tx.clone();
+    let temp_sensor_app = thread::spawn(move || {
+        let msg = BusMessage::Telemetry { sensor_id: 1, value: 1.0 };
         tx.send(msg).unwrap();
     });
 
-    let received = rx.recv().unwrap();
+    let nav_app = thread::spawn(move || {
+        let msg = BusMessage::Command  {
+            target_app: "thruster_app".to_string(),
+            action: "burn".to_string(),
+        };
+        tx2.send(msg).unwrap();
+    });
 
-    // `match` forces you to handle every possible variant.
-    // This is Rust's answer to "what if I forget to handle a message type?"
-    match received {
-        BusMessage::Telemetry { sensor_id, value } => {
-            println!("[main_app] telemetry from sensor {}: {}", sensor_id, value);
-        }
-        BusMessage::Command { target_app, action } => {
-            println!("[main_app] command for {}: {}", target_app, action);
-        }
-        BusMessage::Error(msg) => {
-            println!("[main_app] error: {}", msg);
+    temp_sensor_app.join().unwrap();
+    nav_app.join().unwrap();
+
+    while let Ok(msg) = rx.recv() {
+        match msg {
+            BusMessage::Telemetry { sensor_id, value } => {
+                println!("[{:?}] {:?}", sensor_id, value)
+            }
+            BusMessage::Command { target_app, action } => {
+                println!("[{:?}] {:?}", target_app, action);
+            }
+            BusMessage::Error(msg) => {
+                println!("[{:?}] {:?}", msg, msg);
+            }
         }
     }
-
-    sensor_app.join().unwrap();
 }
