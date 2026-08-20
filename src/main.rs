046847969ede17beb81e3c8b409cd8766f368687
Dutzu05 <vladlusca05@gmail.com>
2026-08-20 12:12:02 +0300

    Lesson 4 - Error manage

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,32 @@ use std::thread;
 #[derive(Debug)]
 enum BusMessage {
     Telemetry { sensor_id: u8, value: f32 },
-    Command { target_app: String, action: String },
-    Error(String),
+}
+
+fn read_sensor(sensor_id: u8) -> Result<BusMessage, String> {
+    if (sensor_id == 0) {
+        return Err(format!("Sensor {} does not exist!", sensor_id));
+    }
+    Ok(BusMessage::Telemetry { sensor_id, value: 1.0 })
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel::<BusMessage>();
-    let tx2 = tx.clone();
-    let temp_sensor_app = thread::spawn(move || {
-        let msg = BusMessage::Telemetry { sensor_id: 1, value: 1.0 };
-        tx.send(msg).unwrap();
-    });
-
-    let nav_app = thread::spawn(move || {
-        let msg = BusMessage::Command  {
-            target_app: "thruster_app".to_string(),
-            action: "burn".to_string(),
-        };
-        tx2.send(msg).unwrap();
-    });
-
-    temp_sensor_app.join().unwrap();
-    nav_app.join().unwrap();
-
-    while let Ok(msg) = rx.recv() {
-        match msg {
-            BusMessage::Telemetry { sensor_id, value } => {
-                println!("[{:?}] {:?}", sensor_id, value)
-            }
-            BusMessage::Command { target_app, action } => {
-                println!("[{:?}] {:?}", target_app, action);
-            }
-            BusMessage::Error(msg) => {
-                println!("[{:?}] {:?}", msg, msg);
+    let sensor_app = thread::spawn(move || {
+        for id in [1, 0] {
+            match read_sensor(id) {
+                Ok(msg) => {
+                    tx.send(msg).unwrap();
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
             }
         }
+    });
+    sensor_app.join().unwrap();
+
+    while let Ok(msg) = rx.recv() {
+        println!("{:?}", msg);
     }
 }
